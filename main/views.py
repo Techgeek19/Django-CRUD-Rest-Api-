51f0ce3b9f50d1b08d9c480e053d02e8b5fc932b
gaurav <gmishra.work@gmail.com>
2020-05-04 16:37:40 +0530

    Search Functionality

--- main/views.py
+++ main/views.py
@@ -8,3 +8,10 @@ from rest_framework import viewsets
 class ProductViewset(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = serializers.ProductSerializer
+    # Searching in Name 
+    def get_queryset(self):
+        pname= self.request.GET.get("pname")
+        if pname== None:
+            pname= ""
+        return Product.objects.filter(name__icontains= pname)
+
