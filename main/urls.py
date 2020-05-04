34f798213906ffe1354380b6fbd3aac7b421476d
gaurav <gmishra.work@gmail.com>
2020-05-04 19:32:41 +0530

    JWT authentication added

--- main/urls.py
+++ main/urls.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from main import views
+from rest_framework_jwt.views import obtain_jwt_token
 
 router = routers.DefaultRouter()
 router.register(r'product', views.ProductViewset)
 
 urlpatterns = [
     path('api/', include(router.urls)),
+    path(r'api-token-auth/', obtain_jwt_token),
 ]
